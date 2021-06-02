176d42fcfbdba3d429850f3962af0b690bc01cf4
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2021-06-02 10:12:42 +0200

    Add Nuget Testing Usage

---
+++ NugetTestingUsage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NugetTestingPackage;
+
+namespace NugetTestingUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new NugetTestingPackageDemo().HelloWorld());
+        }
+    }
+}
